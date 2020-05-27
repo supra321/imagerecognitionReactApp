2c1d896ea06a1202dbd438b4ddaa05be343b118d
supra321 <suprativdatta@gmail.com>
2020-05-28 00:37:58 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/imagerecognitionReactApp/precache-manifest.b2691a5bb927569d34cf1801dbfcfd7d.js"
+  "/imagerecognitionReactApp/precache-manifest.9864123095f6c0ce38e931d3fb37fb8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/imagerecognitionReactApp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
